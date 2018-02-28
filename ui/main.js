3d5b2b64dbbfcca017b1c9e9ed5e79e7bcc80e20
ronirn <31581078+ronirn@users.noreply.github.com>
2018-02-28 21:54:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = "HI";
+var mad = document.getElementById("mad");
+mad.oneclick = function(){
+    mad.style.marginLeft = "100px";
+};
