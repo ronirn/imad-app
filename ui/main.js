869d10d76ea5154ad2d866e059a8175e5c50bf44
ronirn <31581078+ronirn@users.noreply.github.com>
2018-02-28 22:04:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,13 @@ var element = document.getElementById('main-text');
 element.innerHTML = "HI";
 
 var mad = document.getElementById("mad");
+var moveleft =0;
+
+function moveRight(){
+    moveleft = moveleft + 5;
+    img.style.moveleft = moveleft + 'px';
+}
 
 mad.onclick = function(){
-    mad.style.marginLeft = "100px";
+    var interval =  setInterval(moveRight, 50);
 };
