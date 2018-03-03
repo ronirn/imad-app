d3a005b93656e9974b40aa2d2d27d3583df3867f
ronirn <31581078+ronirn@users.noreply.github.com>
2018-03-03 09:34:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,7 +26,7 @@ button.onclick = function(){
         
     };
      
-    request.open('GET', 'http://papai983g.imad.hasura-app.io/',true);
+    request.open('GET', 'http://papai983g.imad.hasura-app.io/counter',true);
     request.send(null);
    
 };
