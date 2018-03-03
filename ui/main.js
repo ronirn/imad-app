1b0e2b3273ab984029839ae140993b510cb9c171
ronirn <31581078+ronirn@users.noreply.github.com>
2018-03-03 09:06:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,16 @@
-console.log('Loaded!');
+// Counter code
 
+var button = document.getElementById("counter");
+var counter = 0;
 
-var element = document.getElementById('main-text');
-
-element.innerHTML = "HI";
-
-var mad = document.getElementById("mad");
-var moveleft =0;
-
-function moveRight(){
-    moveleft = moveleft + 1;
-    mad.style.marginLeft = moveleft + 'px';
-}
-
-mad.onclick = function(){
-    var interval =  setInterval(moveRight, 50);
+button.onclick = function(){
+     
+    // make a request to the counter endponit
+    
+    // capture the response and store it in a variable
+     
+    //render the variable
+    counter += 1;
+    var span = document.getElementById('count');  
+    span.innerHTML = counter.toString();
 };
